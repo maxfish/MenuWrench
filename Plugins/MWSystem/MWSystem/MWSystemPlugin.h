5c8c07eb452fe39916472d3bb3953eb13e592ad0
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2017-04-14 18:24:46 +0200

    New System plugin

---
+++ Plugins/MWSystem/MWSystem/MWSystemPlugin.h
@@ -0,0 +1,14 @@
+//
+//  MWSystemPlugin.h
+//  MWSystemPlugin
+//
+//  Created by maxfish on 26/03/17.
+//  Copyright (c) 2017 Massimiliano Pesce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MWPluginInterface.h"
+
+
+@interface MWSystemPlugin : NSObject <MWPluginInterface>
+@end
---
+++ Plugins/MWSystem/MWSystem/MWSystemPlugin.m
@@ -0,0 +1,101 @@
+//
+//  MWSystemPlugin.m
+//  MWSystemPlugin
+//
+//  Created by maxfish on 26/03/17.
+//  Copyright (c) 2017 Massimiliano Pesce. All rights reserved.
+//
+
+#import "MWSystemPlugin.h"
+#import "SystemDefaults.h"
+
+NSString *const kConfigEnabledKey = @"enable";
+
+@implementation MWSystemPlugin {
+    NSDictionary *_appInfo;
+    BOOL _debugEnabled;
+    NSMutableDictionary *_pluginConfig;
+    NSMenuItem *_menuItem;
+    NSMenu *_submenu;
+
+    SystemDefaults *_defaults;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _pluginConfig = [[NSMutableDictionary alloc] init];
+
+        // Default values
+        _pluginConfig[kConfigEnabledKey] = @(YES);
+        _defaults = [[SystemDefaults alloc] init];
+    }
+
+    return self;
+}
+
+- (BOOL)startWithInfo:(NSDictionary *)appInfo {
+    _appInfo = appInfo;
+    _debugEnabled = [appInfo[kAppInfoDebugEnabled] boolValue];
+    return YES;
+}
+
+- (void)stop {
+
+}
+
+- (void)_createMenuItem {
+    NSString *pluginName = [[NSBundle bundleForClass:self.class] objectForInfoDictionaryKey:kBundlePluginName];
+    _menuItem = [[NSMenuItem alloc] initWithTitle:pluginName action:nil keyEquivalent:@""];
+    _submenu = [[NSMenu alloc] init];
+    [self.menuItem setEnabled:YES];
+
+    [_submenu addItem:[NSMenuItem separatorItem]];
+
+    NSMenuItem *m2 = [_submenu addItemWithTitle:@"Defaults" action:nil keyEquivalent:@""];
+    NSMenu *s1 = [[NSMenu alloc] init];
+
+    // One menu item per setting
+    for (int index = 0; index < _defaults.settings.count; index++) {
+        NSArray *setting = _defaults.settings[(NSUInteger) index];
+        NSMenuItem *menuItem = [s1 addItemWithTitle:setting[0] action:@selector(toggleDefaultsMenuItem:) keyEquivalent:@""];
+        [menuItem setTarget:self];
+        [menuItem setTag:index];
+        [self _setDefaultsMenuItemStatus:menuItem];
+    }
+
+    [m2 setSubmenu:s1];
+    [self.menuItem setSubmenu:_submenu];
+    NSMenuItem *cleanItem = [_submenu addItemWithTitle:@"Clean up 'Open with' associations" action:@selector(cleanOpenWithAssociations:) keyEquivalent:@""];
+    [cleanItem setEnabled:YES];
+    [cleanItem setTarget:self];
+}
+
+- (NSMenuItem *)menuItem {
+    if (!_menuItem) {
+        [self _createMenuItem];
+    }
+
+    return _menuItem;
+}
+
+- (void)_setDefaultsMenuItemStatus:(NSMenuItem *)item {
+    [item setState:[_defaults statusOf:(NSUInteger) item.tag] ? NSOnState : NSOffState];
+}
+
+- (IBAction)toggleDefaultsMenuItem:(id)sender {
+    NSMenuItem *menuItem = sender;
+    NSUInteger default_id = (NSUInteger) menuItem.tag;
+    BOOL newState = ![_defaults statusOf:default_id];
+    [_defaults setDefault:default_id to:newState];
+    [menuItem setState:newState ? NSOnState : NSOffState];
+}
+
+- (void)cleanOpenWithAssociations:(id)sender {
+    NSArray *arguments = @[@"-kill", @"-r", @"-domain", @"local", @"-domain", @"user"];
+    NSTask *task = [NSTask launchedTaskWithLaunchPath:@"/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister" arguments:arguments];
+    [task waitUntilExit];
+    [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/killall" arguments:@[@"Finder"]] waitUntilExit];
+}
+
+@end
